3d67a9d68c50c61c1d1b18c491c5a2d7fecf618c
urtzi88 <urtzip@gmail.com>
2016-04-01 11:02:55 +0200

    exercise complted

---
+++ exercise.rb
@@ -0,0 +1,21 @@
+def split_into_words(input)
+	words = input.split(/ /)
+	remove_punctuation(words.sort)
+end
+
+def remove_punctuation(input)
+	string_np = []
+	input.each do |word|
+		word_np = word.gsub(/[^A-z]/, '')
+		string_np << word_np
+	end
+	custom_sorting(string_np)
+end
+
+def custom_sorting(input)
+	words = input
+	words.sort_by! { |word| word.upcase }
+	puts words
+end
+
+split_into_words("Hello world, blah blah blah; TEST! activity Exercise")
